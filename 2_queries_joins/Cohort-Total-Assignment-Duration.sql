a211e980e45fa1b2514a3d79cda3e924f9b8c556
Hadjira <bkh.hadjira@gmail.com>
2021-07-20 19:15:39 +0000

    update 2_queries_joins

--- 2_queries_joins/Cohort-Total-Assignment-Duration.sql
+++
@@ -1,6 +0,0 @@
-SELECT sum(assignment_submissions.duration) as total
-FROM assignment_submissions
-JOIN students ON students.id = assignment_submissions.student_id 
-JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE students.name = 'Ibrahim Schimmel';
-
